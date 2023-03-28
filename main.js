e58177e728c859a70edf2110b3cf8cdb09159015
kazuki.Y <7777805+kazuki0999@users.noreply.github.com>
2023-03-29 06:03:46 +0900

    test

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 const url2="https://www.google.com";
 
 
-fetch(url2)
+fetch(url2,{
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    mode: 'cors', // no-cors, *cors, same-origin
+    cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+
+    headers: {
+        'Content-Type': 'application/html'
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+    },})
     .then(response => {
         return response.text();
     })

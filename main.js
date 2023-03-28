57dd91a268f51ddf4e0287e11246b97a80764b26
kazuki.Y <7777805+kazuki0999@users.noreply.github.com>
2023-03-29 05:42:09 +0900

    test

--- main.js
+++ main.js
@@ -3,19 +3,12 @@ const url2="https://www.google.com";
 
 fetch(url2)
     .then(response => {
-        console.log(response.headers.get('Access-Control-Allow-Origin'));
-
-        if (response.ok) {
-            return response.text(); // HTMLを取得する
-        } else {
-            throw new Error('Error: Could not retrieve HTML');
-        }
+        return response.text();
     })
     .then(html => {
-        console.log(html); // HTMLを表示する
+        console.log(html);
     })
     .catch(error => {
         console.error(error);
     });
-
 
